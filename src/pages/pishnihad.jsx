a303569a853ed8b03cb6d64be26163048faf1c79
Rahimi <samiullah.rahiimi0909@gmail.com>
2023-06-10 16:05:38 +0430

    Changes created

--- src/pages/pishnihad.jsx
+++ src/pages/pishnihad.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import { useState } from "react";
 import Header from "../components/header";
 import Footer from "../components/footer";
-import { Divider } from "@material-ui/core";
+import { Checkbox, Divider } from "@material-ui/core";
 import { Formik, Field, Form } from "formik";
 import DatePicker from "react-multi-date-picker";
 import arabic from "react-date-object/calendars/arabic";
 import arabic_ar from "react-date-object/locales/arabic_ar";
+import { presidencies } from "./../assets/data/data.js";
 import "./pages.css";
 
 const Maktob = () => {
@@ -15,6 +16,40 @@ const Maktob = () => {
     var input = document.getElementById("maktobDate");
     console.log("Input", input.value);
   };
+
+  const [btnChecked, setBtnChecked] = useState(false);
+  const [selectedPresidencies, setSelectedPresidencies] = useState([]);
+
+  const selectAllPresidencies = () => {
+    if (selectedPresidencies.length === presidencies.length) {
+      // If all checkboxes are already selected, unselect all
+      setSelectedPresidencies([]);
+    } else {
+      // Otherwise, select all checkboxes
+      const allPresidencyLabels = presidencies.map((item) => item.label);
+      setSelectedPresidencies(allPresidencyLabels);
+    }
+  };
+
+  const handleCheckboxChange = (label) => {
+    if (selectedPresidencies.includes(label)) {
+      // If the checkbox is already selected, remove it from the selectedPresidencies state
+      setSelectedPresidencies(
+        selectedPresidencies.filter((item) => item !== label)
+      );
+    } else {
+      // Otherwise, add it to the selectedPresidencies state
+      setSelectedPresidencies([...selectedPresidencies, label]);
+    }
+  };
+
+  const columns = [];
+  const itemsPerColumn = 7;
+
+  for (let i = 0; i < presidencies.length; i += itemsPerColumn) {
+    const columnItems = presidencies.slice(i, i + itemsPerColumn);
+    columns.push(columnItems);
+  }
 
   return (
     <>
@@ -29,6 +64,7 @@ const Maktob = () => {
             maktobDate: "",
             subject: "ALmart",
             context: "contect",
+            recipient: "Recipient",
           }}
           onSubmit={handleChange}
         >
@@ -39,6 +75,15 @@ const Maktob = () => {
                   <div className="form-outline">
                     <label className="form-label mr-3" htmlFor="maktobNo">
                       د پیشنهاد ګڼه/شماره
+                      <span
+                        style={{
+                          color: "red",
+                          marginInline: "5px",
+                          paddingTop: "5px",
+                        }}
+                      >
+                        *
+                      </span>
                     </label>
                     <input
                       type="text"
@@ -57,6 +102,15 @@ const Maktob = () => {
                   <div className="form-outline">
                     <label className="form-label mr-3" htmlFor="maktobDate">
                       نیټه/تاریخ
+                      <span
+                        style={{
+                          color: "red",
+                          marginInline: "5px",
+                          paddingTop: "5px",
+                        }}
+                      >
+                        *
+                      </span>
                     </label>
                     <br />
                     <DatePicker
@@ -75,24 +129,74 @@ const Maktob = () => {
                   </div>
                 </div>
               </div>
-              <div className="row mb-4">
-                <div className="form-outline mb-4 col">
-                  <label className="form-label mr-3" htmlFor="subject">
-                    مخاطب
-                  </label>
-                  <input
-                    type="text"
-                    id="subject"
-                    name="subject"
-                    className="form-control"
-                    value={values.subject}
-                    onChange={(e) => setFieldValue("subject", e.target.value)}
-                    onBlur={() => setFieldTouched("subject", true)}
-                  />
-                </div>
+
+              <div className="row ">
+                {!btnChecked ? (
+                  <div className="form-outline col">
+                    <label className="form-label mr-3" htmlFor="subject">
+                      مخاطب
+                      <span
+                        style={{
+                          color: "red",
+                          marginInline: "5px",
+                          paddingTop: "5px",
+                        }}
+                      >
+                        *
+                      </span>
+                    </label>
+                    <select
+                      // class=" "
+                      style={{ height: "35px" }}
+                      className="form-control form-select-lg mb-3"
+                      aria-label=".form-select-lg example"
+                    >
+                      <option selected>Open this select menu</option>
+                      <option value="1">One</option>
+                      <option value="2">Two</option>
+                      <option value="3">Three</option>
+                    </select>
+                  </div>
+                ) : (
+                  <div className="form-outline  col">
+                    <label className="form-label mr-3" htmlFor="subject">
+                      مخاطب
+                      <span
+                        style={{
+                          color: "red",
+                          marginInline: "5px",
+                          paddingTop: "5px",
+                        }}
+                      >
+                        *
+                      </span>
+                    </label>
+                    <input
+                      type="text"
+                      id="recipient"
+                      name="recipient"
+                      className="form-control"
+                      value={values.recipient}
+                      onChange={(e) =>
+                        setFieldValue("recipient", e.target.value)
+                      }
+                      onBlur={() => setFieldTouched("recipient", true)}
+                    />
+                  </div>
+                )}
+
                 <div className="form-outline mb-4 col">
                   <label className="form-label mr-3" htmlFor="subject">
                     موضوع
+                    <span
+                      style={{
+                        color: "red",
+                        marginInline: "5px",
+                        paddingTop: "5px",
+                      }}
+                    >
+                      *
+                    </span>
                   </label>
                   <input
                     type="text"
@@ -105,9 +209,38 @@ const Maktob = () => {
                   />
                 </div>
               </div>
-              <div className="form-outline mb-4">
+
+              <div class="form-check ">
+                <div style={{ marginTop: "-12px", marginRight: "4px" }}>
+                  <input
+                    class="form-check-input"
+                    type="checkbox"
+                    value=""
+                    id="flexCheckIndeterminate"
+                    onChange={(e) => setBtnChecked(e.target.checked)}
+                  />
+                </div>
+                <label
+                  class="form-check-label"
+                  for="flexCheckIndeterminate"
+                  style={{ marginInline: "24px", fontSize: "11px" }}
+                >
+                  په لست کې شتون نه لري؟
+                </label>
+              </div>
+
+              <div className="form-outline mb-4 mt-5">
                 <label className="form-label mr-3" htmlFor="context">
                   متن
+                  <span
+                    style={{
+                      color: "red",
+                      marginInline: "5px",
+                      paddingTop: "5px",
+                    }}
+                  >
+                    *
+                  </span>
                 </label>
                 <textarea
                   className="form-control"
@@ -120,10 +253,67 @@ const Maktob = () => {
                 ></textarea>
               </div>
 
-              <div className="text-left">
-                <button type="submit" className="btn btn-success button-1">
-                  ثبت او پرنت
-                </button>
+              <div className="row ">
+                <div className="form-outline col">
+                  <label className="form-label mr-3" htmlFor="subject">
+                    استونکی
+                    <span
+                      style={{
+                        color: "red",
+                        marginInline: "5px",
+                        paddingTop: "5px",
+                      }}
+                    >
+                      *
+                    </span>
+                  </label>
+                  <input
+                    type="text"
+                    id="subject"
+                    name="subject"
+                    className="form-control"
+                    value={values.subject}
+                    onChange={(e) => setFieldValue("subject", e.target.value)}
+                    onBlur={() => setFieldTouched("subject", true)}
+                  />
+                </div>
+
+                <div className="form-outline mb-4 col">
+                  <label className="form-label mr-3" htmlFor="subject">
+                    مرجع
+                    <span
+                      style={{
+                        color: "red",
+                        marginInline: "5px",
+                        paddingTop: "5px",
+                      }}
+                    >
+                      *
+                    </span>
+                  </label>
+                  <select
+                    // class=" "
+                    style={{ height: "35px" }}
+                    className="form-control form-select-lg mb-3"
+                    aria-label=".form-select-lg example"
+                  >
+                    <option selected>Open this select menu</option>
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                  </select>
+                </div>
+              </div>
+              <div className="row">
+                <div className="text-left col"></div>
+                <div className="text-left col">
+                  <button type="submit" className="btn bg-primary button-1">
+                    ثبت
+                  </button>
+                  <button type="submit" className="btn bg-primary button-1">
+                    ثبت او پرنت
+                  </button>
+                </div>
               </div>
             </Form>
           )}
