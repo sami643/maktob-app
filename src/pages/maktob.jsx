c45f4810ba14113b29ae42f0646532b645bbd1c0
Rahimi <samiullah.rahimi0909@gmail.com>
2023-06-22 00:23:08 +0430

    maktob header style is updated but not completed

--- src/pages/maktob.jsx
+++ src/pages/maktob.jsx
@@ -172,7 +172,7 @@ const Maktob = (props) => {
 
   return (
     <>
-      {isFormState ? (
+      {!isFormState ? (
         <>
           <Header />
           <div className="main-container text-right">
@@ -187,7 +187,7 @@ const Maktob = (props) => {
                 context: initialValues.context,
               }}
               onSubmit={onSubmitForm_1}
-              validationSchema={maktobValidationSchema}
+              // validationSchema={maktobValidationSchema}
             >
               {({
                 values,
@@ -566,186 +566,200 @@ const Maktob = (props) => {
         </>
       ) : (
         <>
-          <div className="main_container ">
-            <div className="contain maktobviewcontainer mt-5 p-0 " dir="rtl">
-              <div className="header-body ">
-                <div className=" m-0 tvet_logo ">
-                  <img src={Imarat_Logo} alt="" min-width="120" height="100" />
+          <div className="main_container">
+            <div className="header-body">
+              <div className=" pashto_headling_div col-md-4 col-xl-4 col-sm-6">
+                <div className=" pastho-side ">
+                  <div className="  tvet_logo ">
+                    <img src={Imarat_Logo} alt="" min-width="100" height="80" />
+                  </div>
                 </div>
-                <div className="mt-4 pashto_headling_div">
-                  <img src={ImratName_Pashto} className="pashto_headling_img" />
-                  <h4 className="m-0 text-right  pashto_headline_text">
+                <div className="pashto-text-div-inner">
+                  <img src={ImratName_Pashto} className="pashto_heading_img" />
+                  <p className="m-0 text-right  pashto_headline_text">
                     د تخنیکي او مسلکي زده کړو اداره
-                  </h4>
-                </div>
-
-                <div className=" pt-4 english_headline_div">
-                  <h4 className=" text-center m-0 english_headline_text">
-                    Islamic Imarat of Afghanistan Technical & Vocational
-                    Education Training Authority
-                  </h4>
-                </div>
-
-                <div className="mt-4  persian_headline_div">
-                  <img src={ImratName} className="persian_headline_img" />
-                  <h4 className="m-0  persian_headline_text">
-                    اداره تعلیمات تخنیکی و مسلکی
-                  </h4>
-                </div>
-
-                <div className="imarat_logo mr-3">
-                  <img src={Logo} alt="" min-width="120" height="100" />
+                  </p>
                 </div>
               </div>
-              <div className="owner">
+
+              <div className=" pt-4  english_headline_div   col-md-4">
+                <h4 className=" text-center m-0 english_headline_text">
+                  Islamic Imarat of Afghanistan Technical & Vocational Education
+                  Training Authority
+                </h4>
+              </div>
+
+              <div className="col-md-4 col-xl-4 col-sm-6 persian_headline_div">
+                <div className=" persian-text-div-inner ">
+                  <img src={ImratName} className="persian_headline_img " />
+                  <p className="m-0  persian_headline_text">
+                    اداره تعلیمات تخنیکی و مسلکی
+                  </p>
+                </div>
+                <div className="imarat_logo px-0">
+                  <img src={Logo} alt="" min-width="100" height="80" />
+                </div>
+              </div>
+            </div>
+
+            <div className="date_type_no_div   col-12 ">
+              <div className="maktob_no col-4 align-self-end">
+                <label>ګڼه:</label>
+                <p>{formData.maktobNo}</p>
+              </div>
+              <div className="owner col-4">
                 <div>معاونیت امور تعلیمی و تحصیلی</div>
                 <div>ریاست نصاب تعلیمی و تحصیلی</div>
-                <div>آمریت تحلیل و ارزیابی</div>
+                {/* <div>آمریت تحلیل و ارزیابی</div>
                 <div>مدیریت اجراینه</div>
-                <div>مدیریت اجراینه</div>
-              </div>
-              <div className="date">
-                <label htmlFor="">ڼیټه:</label>
-                <p> &#160;{formData.maktobDate ? formData.maktobDate : ""}</p>
+                <div>مدیریت اجراینه</div> */}
               </div>
 
-              <div className="maktob_type_div">
-                <div className="maktob_type_content">
-                  <div className="maktob_no">
-                    <label>ګڼه:</label>
-                    <p>{formData.maktobNo}</p>
-                  </div>
-                  <div className="maktob_type">
-                    <div>
-                      <label className="mx-1">عاجل</label>
+              <div className=" bg-primary col-4 date_type_div align-self-end ">
+                <div className="date d-flex justify-content-end  ">
+                  <label htmlFor="">نیټه:</label>
+                  <p> &#160;{formData.maktobDate}</p>
+                </div>
+                <div className="maktob_type_div d-flex justify-content-end">
+                  <div className="maktob_type text-left">
+                    <div className="px-2">
+                      <label className="">عاجل</label>
                       <Checkbox
                         defaultChecked={false}
                         disabled
-                        className="ml-3"
+                        className="maktob-type-check p-0"
                       />
                     </div>
-                    <div>
-                      <label className="mx-1">محرم</label>
-                      <Checkbox defaultChecked={false} className="ml-3" />
+                    <div className="px-2">
+                      <label className="">محرم</label>
+                      <Checkbox
+                        defaultChecked={false}
+                        className="maktob-type-check p-0"
+                      />
                     </div>
-                    <div>
-                      <label className="mx-1">اطلاعیه</label>
-                      <Checkbox indeterminate disabled className="ml-3" />
+                    <div className="px-2">
+                      <label className="">اطلاعیه</label>
+                      <Checkbox
+                        indeterminate
+                        disabled
+                        className=" maktob-type-check p-0"
+                      />
                     </div>
-                    <div>
-                      <label className="mx-1">سایر موارد</label>
+                    {/* <div className="px-2">
+                      <label className="">سایر موارد</label>
                       <Checkbox
                         defaultChecked={false}
                         disabled
-                        className="ml-3"
+                        className="maktob-type-check p-0"
                       />
-                    </div>
+                    </div> */}
                   </div>
                 </div>
               </div>
-
+            </div>
+            <div>
               <Divider className="divider" />
-              <div className="body_of_maktob ">
-                <p className="audiance">{formData.recipent}</p>
-                <p className="greating">
-                  ٱلسَّلَامُ عَلَيْكُمْ وَرَحْمَةُ ٱللَّهِ وَبَرَكَاتُهُ ً{" "}
-                </p>
-                <div className="subject_of_maktob">
-                  <label> موضوع </label>
-                  <p>:&#160;&#160; {formData.subject} </p>
-                </div>
-                <p>محترما:</p>
-                <p className="matktob_context">{formData.context}</p>
-                <br />
-                <div className="closing_signature">
-                  <p>والسلام</p>
-                  <p>انجنیر شریف احمد راسخ</p>
-                  <p>آمر سیستم های معلوماتی و احصایه</p>
-                </div>
+            </div>
+            <div className="body_of_maktob ">
+              <p className="audiance">{formData.recipent}</p>
+              <p className="greating">
+                ٱلسَّلَامُ عَلَيْكُمْ وَرَحْمَةُ ٱللَّهِ وَبَرَكَاتُهُ ً{" "}
+              </p>
+              <div className="subject_of_maktob">
+                <label> موضوع </label>
+                <p>:&#160;&#160; {formData.subject} </p>
               </div>
-
-              <div className="copy_to_div ">
-                <p className="copy_to_title">کاپي به:</p>
-                <div className="copy_to_body">
-                  {isDeputOrAdvisoryChecked ? (
-                    <>
-                      <div className="copy_body_item">
-                        {arrayA.map((item, index) => (
-                          <p key={index}>{item}</p>
-                        ))}
-                      </div>
-                      <div className="copy_body_item">
-                        {arrayB.map((item, index) => (
-                          <p key={index}>{item}</p>
-                        ))}
-                      </div>
-
-                      <div className="copy_body_item">
-                        {arrayC.map((item, index) => (
-                          <p key={index}>{item}</p>
-                        ))}
-                      </div>
-                    </>
-                  ) : (
-                    <>
-                      {" "}
-                      <div className="copy_body_item">
-                        {copyToRecipentsJustLabel_1
-                          .slice(0, 8)
-                          .map((item, index) => (
-                            <p key={index}>{item}</p>
-                          ))}
-                      </div>
-                      <div className="copy_body_item">
-                        {copyToRecipentsJustLabel_1
-                          .slice(8, 16)
-                          .map((item, index) => (
-                            <p key={index}>{item}</p>
-                          ))}
-                      </div>
-                      <div className="copy_body_item">
-                        {copyToRecipentsJustLabel_1
-                          .slice(16, 24)
-                          .map((item, index) => (
-                            <p key={index}>{item}</p>
-                          ))}
-                      </div>
-                    </>
-                  )}
-                </div>
-              </div>
-
-              <Divider className="footer_divider" />
-              <div className="footer">
-                <div className="footer-content_maktob">
-                  <div className="footer-item">
-                    آدرس: کارته چهار، جوار وزارت تحصیلات عالی- کابل- افغانستان
-                  </div>
-                  <div className="footer-item">Email: mis_it@tveta.gov</div>
-                  <div className="footer-item">Tel: + 93794465693</div>
-                </div>
+              <p>محترما:</p>
+              <p className="matktob_context">{formData.context}</p>
+              <br />
+              <div className="closing_signature">
+                <p>والسلام</p>
+                <p>انجنیر شریف احمد راسخ</p>
+                <p>آمر سیستم های معلوماتی و احصایه</p>
               </div>
             </div>
 
-            <div className="container print_btn_div ">
-              <button
-                onClick={() => {
-                  setIsFromState(true);
-                }}
-                className="print-button btn bg-primary px-۲"
-              >
-                مخکنۍ صفحه/ صفحه قبلی
-              </button>
+            <div className="copy_to_div ">
+              <p className="copy_to_title">کاپي به:</p>
+              <div className="copy_to_body">
+                {isDeputOrAdvisoryChecked ? (
+                  <>
+                    <div className="copy_body_item">
+                      {arrayA.map((item, index) => (
+                        <p key={index}>{item}</p>
+                      ))}
+                    </div>
+                    <div className="copy_body_item">
+                      {arrayB.map((item, index) => (
+                        <p key={index}>{item}</p>
+                      ))}
+                    </div>
 
-              <button className="print-button btn bg-primary px-5">ثبت</button>
-
-              <button
-                onClick={handlePrint}
-                className="print-button btn bg-primary px-5"
-              >
-                پرنت
-              </button>
+                    <div className="copy_body_item">
+                      {arrayC.map((item, index) => (
+                        <p key={index}>{item}</p>
+                      ))}
+                    </div>
+                  </>
+                ) : (
+                  <>
+                    {" "}
+                    <div className="copy_body_item">
+                      {copyToRecipentsJustLabel_1
+                        .slice(0, 8)
+                        .map((item, index) => (
+                          <p key={index}>{item}</p>
+                        ))}
+                    </div>
+                    <div className="copy_body_item">
+                      {copyToRecipentsJustLabel_1
+                        .slice(8, 16)
+                        .map((item, index) => (
+                          <p key={index}>{item}</p>
+                        ))}
+                    </div>
+                    <div className="copy_body_item">
+                      {copyToRecipentsJustLabel_1
+                        .slice(16, 24)
+                        .map((item, index) => (
+                          <p key={index}>{item}</p>
+                        ))}
+                    </div>
+                  </>
+                )}
+              </div>
             </div>
+
+            <Divider className="footer_divider" />
+            <div className="footer">
+              <div className="footer-content_maktob">
+                <div className="footer-item">
+                  آدرس: کارته چهار، جوار وزارت تحصیلات عالی- کابل- افغانستان
+                </div>
+                <div className="footer-item">Email: mis_it@tveta.gov</div>
+                <div className="footer-item">Tel: + 93794465693</div>
+              </div>
+            </div>
+          </div>
+
+          <div className="container print_btn_div ">
+            <button
+              onClick={() => {
+                setIsFromState(true);
+              }}
+              className="print-button btn bg-primary px-۲"
+            >
+              مخکنۍ صفحه/ صفحه قبلی
+            </button>
+
+            <button className="print-button btn bg-primary px-5">ثبت</button>
+
+            <button
+              onClick={handlePrint}
+              className="print-button btn bg-primary px-5"
+            >
+              پرنت
+            </button>
           </div>
         </>
       )}
